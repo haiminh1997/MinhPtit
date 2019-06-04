b5503560df84af84d557ab1d45ed56f03b3a9408
haiminh1997 <haiminh97lqd@gmail.com>
2019-06-04 18:47:16 +0700

    btvn

---
+++ PHP/PHP Basic/COOKIE-SESSION/T3H/index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HaiMinh
+ * Date: 6/4/2019
+ * Time: 6:40 PM
+ */
+//Khởi động session
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h1> Khởi động session</h1>
+</body>
+</html>
